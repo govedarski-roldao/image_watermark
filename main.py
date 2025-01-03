e01a57a64ed32f976ce56f320572785816fdee0c
Rodolfo Govedarski-Roldao <r_roldao@hotmail.com>
2025-01-03 23:58:38 +0200

    add the watermark functionality

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from tkinter import *
 from tkinter import filedialog, messagebox
-from PIL import Image, ImageTk
+from PIL import Image, ImageDraw, ImageFont, ImageTk
 
 current_image = None
 
@@ -12,11 +12,11 @@ def choose_image():
         field_url.config(text=f"File chosen: {aplication}")
         try:
             img = Image.open(aplication)
-            img = img.resize((200, 200))
+            img = img.resize((300, 300))
             current_image = img
             tk_img = ImageTk.PhotoImage(img)
             image_area.image = tk_img
-            image_area.create_image(100, 100, image=tk_img)
+            image_area.create_image(150, 150, image=tk_img)
         except Exception as e:
             field_url.config(text=f"Error loading image: {e}")
 
@@ -26,12 +26,30 @@ def choose_image():
 
 def add_watermark():
     global current_image
+    max_characters = 10
     text = watermark_text_field.get()
     if text:
-        if current_image:
-            pass
+        if len(text) > max_characters:
+            messagebox.showinfo(title="Watermark Error", message=f"The watermark needs to have max. {max_characters} characters")
+        else:
+            if current_image:
+                try:
+                    txt = Image.new("RGBA", current_image.size, (255, 255, 255, 0))
+                    d = ImageDraw.Draw(txt)
+                    font = ImageFont.truetype("arial.ttf", 26)
+
+                    d.text((100, 100), text=text, font=font, fill=(255, 255, 255, 128))
+
+                    watermarked_image = Image.alpha_composite(current_image.convert("RGBA"), txt)
+
+                    tk_img = ImageTk.PhotoImage(watermarked_image.resize((300, 300)))
+                    image_area.image = tk_img
+                    image_area.create_image(150, 150, image=tk_img)
+                except Exception as e:
+                    messagebox.showerror(title="Watermark Error", message=f"An error occurred: {e}")
     else:
-        messagebox.showinfo(title="Watermark Error")
+        messagebox.showinfo(title="Watermark Error", message="You need to insert some text on the watermark field")
+
 
 # region UI
 # ---------------------------------    window
@@ -43,7 +61,7 @@ root.config(padx=50, pady=50)
 # ---------------------------------    Labels
 
 # image area
-image_area = Canvas(width=200, height=200, bg="white", highlightthickness=5)
+image_area = Canvas(width=300, height=300, bg="white", highlightthickness=5)
 image_area.grid(column=0, row=0, columnspan=2, rowspan=7)
 
 # search title
@@ -65,7 +83,7 @@ watermark_text_field.grid(column=2, row=5)
 add_image = Button(text="Upload Image", padx=20, command=choose_image)
 add_image.grid(column=2, row=3)
 
-apply_button = Button(text="Create Watermark", padx=20)
+apply_button = Button(text="Create Watermark", padx=20, command=add_watermark)
 apply_button.grid(column=2, row=6)
 
 root.mainloop()
